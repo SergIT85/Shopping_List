a8a94ddd59d44049c07e325768ded2f13ddd8c35
Sergey Korchagin <kor.work@mail.ru>
2023-02-28 09:22:11 +0800

    5. Finished MVVM 2 screen

--- app/src/main/java/ru/androideducation/shopping_list/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/androideducation/shopping_list/data/ShopListRepositoryImpl.kt
@@ -15,7 +15,7 @@ object ShopListRepositoryImpl: ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for(i in 0..1000) {
+        for(i in 0..10) {
             val item = ShopItem("Item ${i+1}", i, Random.nextBoolean())
             addShopItem(item)
         }
